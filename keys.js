76bf663ec5cacc7b8747bcc70ea6345fc9953069
John Webster <JohnLobster@comcast.net>
2019-05-12 13:49:08 -0700

    spotify keys working

--- keys.js
+++ keys.js
@@ -1,6 +1,7 @@
 // supplies key information for spotify
-console.log('this is loaded');
-
+require("dotenv").config({ debug: process.env.DEBUG });
+// console.log('Loading keys');
+// console.log("Loading keys " + process.env.SPOTIFY_ID + " : " + process.env.SPOTIFY_SECRET);
 exports.spotify = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
